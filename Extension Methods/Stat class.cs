61d3fe7040e0d7621f7b8f071d915e1d21c3d1d8
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-06 20:27:08 +0530

    Add files via upload

---
+++ Extension Methods/Stat class.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension_methods
+{
+    static class Stat_class
+    {
+        public static void Test3(this Program p, int i)
+        {
+            Console.WriteLine("Method 3:" +i);
+        }
+        public static long Factorial(this Int32 x)
+        {
+            if(x == 1)
+                return 1;
+            if (x == 2)
+                return 2;
+            else
+                return x * Factorial(x - 1);
+
+        }
+        public static string ToProper(this string Oldstr)
+        {
+            if(Oldstr.Trim().Length > 0)
+            {
+                string Newstr = null;
+                Oldstr = Oldstr.ToLower();
+                string[] sarr = Oldstr.Split(' ');
+                foreach(string str in sarr)
+                {
+                    char[] carr = str.ToCharArray();
+                    carr[0] = char.ToUpper(carr[0]);
+                    if (Newstr == null)
+
+                        Newstr = new string(carr);
+                    else
+                        Newstr += " " +new string(carr);
+
+                }
+                return Newstr;
+
+            }
+            return Oldstr;
+        }
+    }
+}
---
+++ Extension Methods/TestExt Methods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Extension_methods
+{
+    class TestExt_Methods
+    {
+      static void Main()
+        {
+            Program p = new Program();
+            p.Test1();
+            p.Test2();
+            p.Test3(10);
+
+            int i = 5;
+            long result = i.Factorial();
+            Console.WriteLine("Factorial of {0} is: {1}", i, result);
+
+            String str = "hEILo hI";
+            str = str.ToProper();
+            Console.WriteLine(str);
+            
+        }
+    }
+}
